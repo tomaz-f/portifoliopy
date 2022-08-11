bc39d615ebc5d0181b0e75b8a0f9f2739008c656
Felipe Tomaz <tomazalvesfelipe@gmail.com>
2022-08-11 16:51:45 -0400

    Firts implement of django

---
+++ projects/models.py
@@ -0,0 +1,8 @@
+from django.db import models
+
+# Create your models here.
+class Projects(models.Models):
+    title = models.CharField(max_length=100)
+    description = models.TextField()
+    technology = models.CharField(max_length=20)
+    image = models.FilePathField(path="/img")
