bcadebf2b8f19c02c5d6e3a5793a0218b95e0f65
Felipe Tomaz <tomazalvesfelipe@gmail.com>
2022-08-12 15:25:37 -0400

    add blog_index query the Post models and retrieve all its objects:

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from blog.models import Post, Comment
 
-# Create your views here.
+def blog_index(request):
+    post = Post.objects.all().order_by('-created_on')
+    context = {
+        "posts": posts,
+    }
+    return render(request, "blog_index.html", context)
