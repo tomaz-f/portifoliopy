26379f2ad74ef038d7ab0db1410b5144023af5dd
Felipe Tomaz <tomazalvesfelipe@gmail.com>
2022-08-13 08:39:56 -0400

    add comments section

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from blog.models import Post, Category, Comment
+from .forms import CommentForm
 
 def blog_index(request):
     post = Post.objects.all().order_by('-created_on')
@@ -22,9 +23,22 @@ def blog_category(request, category):
 
 def blog_detail(request, pk):
     post = Post.objects.get(pk=pk)
+
+    form = CommentForm()
+    if request.method == 'POST':
+        form = CommentForm(request.POST)
+        if form.is_valid():
+            comment = Comment(
+                author=form.cleaned_data["author"],
+                body=form.cleaned_data["body"],
+                post=post
+            )
+            comment.save()
+
     comments = Comment.objects.filter(post=post)
     context = {
         "post": posts,
         "comments": comments,
+        "form": form,
     }
     return render(request, "blog_detail.html", context)
