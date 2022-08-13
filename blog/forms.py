1d05b979b486845e468a77af3b27dd12be1a510d
Felipe Tomaz <tomazalvesfelipe@gmail.com>
2022-08-13 08:55:00 -0400

    creat urls routes

--- blog/forms.py
+++ blog/forms.py
@@ -1,16 +1,17 @@
 from django import forms
 
-class CommentForm(forms.Form)
-    author = forms.Charfield(
+
+class CommentForm(forms.Form):
+    author = forms.CharField(
         max_length=60,
         widget=forms.TextInput(attrs={
             "class": "form-control",
             "placeholder": "Your Name"
         })
     )
-    body = forms.Charfield(widget=forms.Textarea(
+    body = forms.CharField(widget=forms.Textarea(
         attrs={
-            "class": "form-control"
+            "class": "form-control",
             "placeholder": "Leave a comment!"
         })
     )
